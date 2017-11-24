be224f1bab540132b06aecad57cccdcf2d0b3ab5
duzc2dtw@gmail.com <duzc2dtw@gmail.com>
2017-11-25 00:32:45 +0800

    beauty code

--- blocks/variables_dynamic.js
+++ blocks/variables_dynamic.js
@@ -44,41 +44,41 @@ Blockly.Constants.VariablesDynamic.HUE = 310;
 
 Blockly.defineBlocksWithJsonArray([ // BEGIN JSON EXTRACT
     // Block for variable getter.
-    {
-        "type": "variables_get_dynamic",
-        "message0": "%1",
-        "args0": [{
-            "type": "field_variable",
-            "name": "VAR",
-            "variable": "%{BKY_VARIABLES_DEFAULT_NAME}"
-        }],
-        "output": null,
-        "colour": "%{BKY_VARIABLES_DYNAMIC_HUE}",
-        "helpUrl": "%{BKY_VARIABLES_GET_HELPURL}",
-        "tooltip": "%{BKY_VARIABLES_GET_TOOLTIP}",
-        "extensions": ["contextMenu_variableDynamicSetterGetter"]
-    },
+  {
+    "type": "variables_get_dynamic",
+    "message0": "%1",
+    "args0": [{
+      "type": "field_variable",
+      "name": "VAR",
+      "variable": "%{BKY_VARIABLES_DEFAULT_NAME}"
+    }],
+    "output": null,
+    "colour": "%{BKY_VARIABLES_DYNAMIC_HUE}",
+    "helpUrl": "%{BKY_VARIABLES_GET_HELPURL}",
+    "tooltip": "%{BKY_VARIABLES_GET_TOOLTIP}",
+    "extensions": ["contextMenu_variableDynamicSetterGetter"]
+  },
     // Block for variable setter.
+  {
+    "type": "variables_set_dynamic",
+    "message0": "%{BKY_VARIABLES_SET}",
+    "args0": [{
+      "type": "field_variable",
+      "name": "VAR",
+      "variable": "%{BKY_VARIABLES_DEFAULT_NAME}"
+    },
     {
-        "type": "variables_set_dynamic",
-        "message0": "%{BKY_VARIABLES_SET}",
-        "args0": [{
-                "type": "field_variable",
-                "name": "VAR",
-                "variable": "%{BKY_VARIABLES_DEFAULT_NAME}"
-            },
-            {
-                "type": "input_value",
-                "name": "VALUE"
-            }
-        ],
-        "previousStatement": null,
-        "nextStatement": null,
-        "colour": "%{BKY_VARIABLES_DYNAMIC_HUE}",
-        "tooltip": "%{BKY_VARIABLES_SET_TOOLTIP}",
-        "helpUrl": "%{BKY_VARIABLES_SET_HELPURL}",
-        "extensions": ["contextMenu_variableDynamicSetterGetter"]
+      "type": "input_value",
+      "name": "VALUE"
     }
+        ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "%{BKY_VARIABLES_DYNAMIC_HUE}",
+    "tooltip": "%{BKY_VARIABLES_SET_TOOLTIP}",
+    "helpUrl": "%{BKY_VARIABLES_SET_HELPURL}",
+    "extensions": ["contextMenu_variableDynamicSetterGetter"]
+  }
 ]); // END JSON EXTRACT (Do not delete this comment.)
 
 /**
@@ -96,39 +96,41 @@ Blockly.Constants.VariablesDynamic.CUSTOM_CONTEXT_MENU_VARIABLE_GETTER_SETTER_MI
      * @param {!Array} options List of menu options to add to.
      * @this Blockly.Block
      */
-    customContextMenu: function(options) {
+  customContextMenu: function(options) {
         // Getter blocks have the option to create a setter block, and vice versa.
-        if (this.type == 'variables_get_dynamic') {
-            var opposite_type = 'variables_set_dynamic';
-            var contextMenuMsg = Blockly.Msg.VARIABLES_GET_CREATE_SET;
-        } else {
-            var opposite_type = 'variables_get_dynamic';
-            var contextMenuMsg = Blockly.Msg.VARIABLES_SET_CREATE_GET;
-        }
-
-        var option = { enabled: this.workspace.remainingCapacity() > 0 };
-        var name = this.getFieldValue('VAR');
-        option.text = contextMenuMsg.replace('%1', name);
-        var xmlField = goog.dom.createDom('field', null, name);
-        xmlField.setAttribute('name', 'VAR');
-        var variableModel = this.workspace.getVariable(name);
-        xmlField.setAttribute('variabletype', variableModel.type);
-        var xmlBlock = goog.dom.createDom('block', null, xmlField);
-        xmlBlock.setAttribute('type', opposite_type);
-        option.callback = Blockly.ContextMenu.callbackFactory(this, xmlBlock);
-        options.push(option);
-    },
-    onchange: function(evt) {
-        var name = this.getFieldValue('VAR');
-        var variableModel = this.workspace.getVariable(name);
-        this.getField("VAR").variableTypes = [variableModel.type];
-        if (this.type == 'variables_get_dynamic') {
-            this.outputConnection.setCheck(variableModel.type);
-        } else {
-            this.getInput("VALUE").connection.setCheck(variableModel.type);
-        }
-
+    var opposite_type ;
+    var contextMenuMsg ;
+    if (this.type == 'variables_get_dynamic') {
+      opposite_type = 'variables_set_dynamic';
+      contextMenuMsg = Blockly.Msg.VARIABLES_GET_CREATE_SET;
+    } else {
+      opposite_type = 'variables_get_dynamic';
+      contextMenuMsg = Blockly.Msg.VARIABLES_SET_CREATE_GET;
     }
+
+    var option = { enabled: this.workspace.remainingCapacity() > 0 };
+    var name = this.getFieldValue('VAR');
+    option.text = contextMenuMsg.replace('%1', name);
+    var xmlField = goog.dom.createDom('field', null, name);
+    xmlField.setAttribute('name', 'VAR');
+    var variableModel = this.workspace.getVariable(name);
+    xmlField.setAttribute('variabletype', variableModel.type);
+    var xmlBlock = goog.dom.createDom('block', null, xmlField);
+    xmlBlock.setAttribute('type', opposite_type);
+    option.callback = Blockly.ContextMenu.callbackFactory(this, xmlBlock);
+    options.push(option);
+  },
+  onchange: function() {
+    var name = this.getFieldValue('VAR');
+    var variableModel = this.workspace.getVariable(name);
+    this.getField("VAR").variableTypes = [variableModel.type];
+    if (this.type == 'variables_get_dynamic') {
+      this.outputConnection.setCheck(variableModel.type);
+    } else {
+      this.getInput("VALUE").connection.setCheck(variableModel.type);
+    }
+
+  }
 };
 
 Blockly.Extensions.registerMixin('contextMenu_variableDynamicSetterGetter',
